00cc7374b59fe0b2d8b5c8ef3ab546c71dee500e
jxpsert <jplatenburg@outlook.com>
2024-08-04 22:57:36 +0200

    Changed page title for login

--- resources/views/pages/auth/layout.blade.php
+++ resources/views/pages/auth/layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Capolyo</title>
+    <title>{{ config('app.name') }}</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
