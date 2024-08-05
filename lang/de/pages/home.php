0797372f795a199ecb1296549af890fa2023d037
jxpsert <jplatenburg@outlook.com>
2024-08-06 00:00:50 +0200

    Checked German translation; Fixed wrong comparison operator

--- lang/de/pages/home.php
+++ lang/de/pages/home.php
@@ -6,7 +6,7 @@ return [
     'hero-text' => 'Mit Hilfe von crowdsourced Informationen von OpenStreetMap arbeitet OpenAED an einer Karte aller AEDs in Europa. Diese Karte ist für jeden verfügbar und kann verwendet werden, um im Notfall den nächstgelegenen AED zu finden*.
     <br><br>
     Darüber hinaus hilft OpenAED bei der Verbreitung von Wissen über AEDs und deren Verwendung. Wir fördern auch die Installation neuer AEDs.',
-    'hero-disclaimer' => '* OpenAED ist nicht verantwortlich für falsche Informationen, obwohl wir unser Bestes tun.',
+    'hero-disclaimer' => '* OpenAED ist nicht verantwortlich für falsche Informationen, obwohl wir unser Bestes dagegen tun.',
 
     'what-is-aed' => 'Was ist ein AED?',
     'to-map' => 'Zur Karte',
@@ -16,17 +16,17 @@ return [
     'view-all-posts' => 'Alle Beiträge anzeigen',
     'no-posts' => 'Es gibt keine Beiträge zum Anzeigen.',
 
-    'what-is-aed-text' => 'Ein automatisierter externer Defibrillator (AED) ist ein tragbares Gerät, das den Herzrhythmus überprüft und einen elektrischen Schock an das Herz senden kann, um zu versuchen, einen normalen Rhythmus wiederherzustellen. AEDs werden zur Behandlung eines plötzlichen Herzstillstands eingesetzt. Ein plötzlicher Herzstillstand ist ein Zustand, bei dem das Herz plötzlich und unerwartet aufhört zu schlagen. Wenn dies passiert, hört das Blut auf, zum Gehirn und anderen lebenswichtigen Organen zu fließen. Ein plötzlicher Herzstillstand ist in der Regel tödlich, wenn er nicht innerhalb von Minuten behandelt wird. Tatsächlich reduziert jede Minute Verzögerung die Überlebenschancen um 7 bis 10 Prozent.
+    'what-is-aed-text' => 'Ein automatisierter externer Defibrillator (AED) ist ein tragbares Gerät, das den Herzrhythmus überprüft und einen elektrischen Schock an das Herz senden kann, um zu versuchen, einen normalen Rhythmus wiederherzustellen. AEDs werden zur Behandlung eines plötzlichen Herzstillstandes eingesetzt. Ein plötzlicher Herzstillstand ist ein Zustand, bei dem das Herz plötzlich und unerwartet aufhört zu schlagen. Wenn dies passiert, kann das Blut nicht mehr zum Gehirn und anderen lebenswichtigen Organen fließen. Ein plötzlicher Herzstillstand ist in der Regel tödlich, wenn er nicht innerhalb von Minuten behandelt wird. Jede verzögerte Minute reduziert die Überlebenschance um 7 bis 10 Prozent.
     <br><br>
-    AEDs sind für die Verwendung durch jedermann konzipiert, auch wenn Sie keine medizinische Ausbildung haben. Der AED führt Sie durch den Vorgang und gibt Ihnen gesprochene Anweisungen. Es ist wichtig zu wissen, wo sich der nächste AED befindet, damit Sie im Notfall schnell handeln können.',
+    AEDs sind für die Verwendung von Jedem konzipiert, auch wenn Sie keine medizinische Ausbildung haben. Der AED führt Sie durch den Vorgang und gibt Ihnen gesprochene Anweisungen. Es ist wichtig zu wissen, wo sich der nächste AED befindet, damit Sie im Notfall schnell handeln können.',
 
     'how-can-i-help' => 'Wie kann ich helfen?',
     'how-can-i-help-map-title' => 'AEDs auf die Karte setzen',
     'how-can-i-help-map-text' => 'Da OpenAED auf Daten von OpenStreetMap angewiesen ist, können Sie helfen, indem Sie AEDs zu OpenStreetMap hinzufügen. Auf diese Weise sind die Daten für OpenAED und andere Dienste verfügbar, die OpenStreetMap-Daten verwenden.',
 
     'how-can-i-help-learn-cpr-title' => 'Lernen Sie CPR',
-    'how-can-i-help-learn-cpr-text' => 'Die Herz-Lungen-Wiederbelebung (HLW) ist eine lebensrettende Technik, die bei vielen Notfällen nützlich ist, einschließlich Herzinfarkt oder beinahe Ertrinken, bei denen die Atmung oder der Herzschlag einer Person aufgehört hat. Der Schlüsselpunkt ist, den Blutfluss aktiv zu halten, bis die Rettungskräfte eintreffen.',
+    'how-can-i-help-learn-cpr-text' => 'Die Herz-Lungen-Wiederbelebung (HLW) ist eine lebensrettende Technik, die bei vielen Notfällen nützlich ist, einschließlich Herzinfarkt oder beinahes Ertrinken, bei denen die Atmung oder der Herzschlag einer Person aufgehört hat. Das wichtigste ist, den Blutfluss aktiv zu halten, bis die Rettungskräfte eintreffen.',
 
-    'how-can-i-help-tell-others-title' => 'Erzählen Sie anderen',
-    'how-can-i-help-tell-others-text' => 'Erzählen Sie Ihren Kollegen, Freunden und Ihrer Familie von der Wichtigkeit, zu wissen, wie zu handeln ist, wenn ein Notfall eintritt. Die zivile Intervention ist der erste Schritt, um Leben zu retten.',
+    'how-can-i-help-tell-others-title' => 'Informeren Sie anderen',
+    'how-can-i-help-tell-others-text' => 'Informeren Sie Ihren Kollegen, Freunden und Ihrer Familie von der Wichtigkeit, zu wissen wie zu handeln ist, wenn ein Notfall eintritt. Die zivile Intervention ist der erste Schritt, um Leben zu retten.',
 ];
