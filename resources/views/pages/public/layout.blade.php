760cfe2500a874d8643b1b9cd78a3e0accfac993
jxpsert <jplatenburg@outlook.com>
2024-08-06 12:04:43 +0200

    Add posts to navbar

--- resources/views/pages/public/layout.blade.php
+++ resources/views/pages/public/layout.blade.php
@@ -35,7 +35,11 @@
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+                            <a class="nav-link" aria-current="page" href="{{ route('home') }}">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page"
+                                href="{{ route('posts.index') }}">{{ __('Posts') }}</a>
                         </li>
                     </ul>
                     <div id="lang-switch" class="d-flex ms-auto lang-switch-container">
